24048131cc86da2c491b6ab55ebdb0efa7236e41
Carlos Tavares <neno.tavares@gmail.com>
2021-09-13 17:40:35 +0000

    copying files and folders

---
+++ copycat01.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+import shutil
+import os
+os.chdir('/home/student/mycode/')
+# The following line will create the directory if it does not exist already
+shutil.copy('5g_research/sdn_network.txt', '5g_research/sdn_network.txt.copy')
+shutil.copytree('5g_research/', '5g_research_backup/')
