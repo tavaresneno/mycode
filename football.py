361259c425dd5bb6b4dc54b9c253d8e6a789ba83
Carlos Tavares <neno.tavares@gmail.com>
2021-09-13 18:47:06 +0000

    Lab9

---
+++ football.py
@@ -0,0 +1,10 @@
+footballstats= [
+    {"Name": "Jameis Winston",
+    "TD": 5,
+    "Pass Yds": 148,
+    "Team": "New Orleans Saints"}, 
+    {"Name": "Taysom Hill",
+    "TD": 0,
+    "Pass Yds": 3,
+    "Team": "New Orleans Saints"}]
+print(footballstats[0]['TD'])
