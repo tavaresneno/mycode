add5802837a043f70ccd2ad957f2f3a29c19c716
Carlos Tavares <neno.tavares@gmail.com>
2021-09-14 14:20:42 +0000

    your commit message

---
+++ paramikosftp/sftpmover_03.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+## Moving files with SFTP
+
+## import paramiko so we can talk SSH
+import paramiko
+import os
+import getpass
+
+## where to connect to
+t = paramiko.Transport("10.10.2.3", 22) ## IP and port
+
+user = input('user name: ')
+
+upass = getpass.getpass('Password:')
+#upass = input(' Password : ') 
+
+
+## how to connect (see other labs on using id_rsa private/public keypairs)
+t.connect(username=user, password=upass)
+
+## Make an sftp connection object
+sftp = paramiko.SFTPClient.from_transport(t)
+
+## iterate across the files within directory
+for x in os.listdir("/home/student/filestocopy/"): # iterate on directory contents
+  if not os.path.isdir("/home/student/filestocopy/"+x): # filter everything that is NOT a directory
+    sftp.put("/home/student/filestocopy/"+x, "/tmp/"+x) # move file to target location
+
+## close the connection
+sftp.close() # close the connection
+
